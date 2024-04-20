e0284cf7f6484bec58c203b1f65d429d9decf56b
priyankabalodi <nareshchandra4763@gmail.com>
2024-04-20 13:13:01 +0530

    cargo

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Home = () =>{
+  
+  return(
+    
+<div className=' h-screen bg-pink-400'>
+<h className="text-white flex justify-center text-center">Welcome to Cargo!!</h>
+<img src='\cargo.svg' alt='img'></img>
+</div>
+
+  )
+}
+export default Home;
